adde7c622e8a2b91feb1f2d858eed952e2741460
Md Monowarul Islam <monowarul7ii@gmail.com>
2025-08-12 19:54:49 +0600

    solved 1370A

---
+++ 1370A.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+def print_fast(x):
+    sys.stdout.write(f"{x}\n")
+
+
+def solve():
+    n = int(input())
+    if n < 3:
+        return 1
+    else:
+        return n//2
+
+
+for _ in range(int(input())):
+    print_fast(solve())
