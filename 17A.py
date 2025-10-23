19dc2e50ac8ba29155f9de66c9efd0dad9d3d653
Md Monowarul Islam <monowarul7ii@gmail.com>
2025-10-23 11:43:31 +0600

    solved 17A

--- 17A.py
+++ 17A.py
@@ -9,8 +9,38 @@ def print_fast(x):
     sys.stdout.write(f"{x}\n")
 
 
+def sieve():
+    primes = [1]*1007
+    primes[0] = primes[1] = 0
+    p = 2
+    while p*p <= 1007:
+        if primes[p]:
+            for i in range(p*p, 1007, p):
+                primes[i] = 0
+        p += 1
+    prime_numbers = []
+    for i in range(1007):
+        if primes[i]:
+            prime_numbers.append(i)
+    return primes, prime_numbers
+
+
 def solve():
+    primes, prime_numbers = sieve()
+    n, k = [int(x) for x in input().split()]
+    ans = 0
+    for i in range(len(primes)-1):
+        # print(prime_numbers[i] + prime_numbers[i+1] + 1)
+        # print(primes[prime_numbers[i] + prime_numbers[i+1] + 1])
+        if prime_numbers[i] + prime_numbers[i+1] + 1 <= n:
+            if primes[prime_numbers[i] + prime_numbers[i+1] + 1]:
+                ans += 1
+        else:
+            break
+    if ans >= k:
+        return 'YES'
+    return 'NO'
 
 
-for _ in range(int(input())):
-    print_fast(solve())
+# for _ in range(int(input())):
+print_fast(solve())
