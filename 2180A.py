1ae83c571c3712dbeb801aff333c103746ec627c
Md Monowarul Islam <monowarul7ii@gmail.com>
2025-12-25 15:46:51 +0600

    solved 2180A

--- 2180A.py
+++ 2180A.py
@@ -13,6 +13,18 @@ def solve():
     l, a, b = [int(x) for x in input().split()]
     ans = l - math.gcd(l, b) + a % math.gcd(l, b)
     return ans
+    # logn solve
+    
+    # seen = set()
+    # curr = a
+    # mx = 0
+    # while curr not in seen:
+    #     seen.add(curr)
+    #     mx = max(mx, curr)
+    #     curr = (curr + b) % l
+    # return mx
+    # linear solve
+
 
 for _ in range(int(input())):
     print_fast(solve())
