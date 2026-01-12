30cf93c32e53dec9f4d751cacbb584e4f2a532fd
Md Monowarul Islam <monowarul7ii@gmail.com>
2026-01-12 13:19:20 +0600

    solved 2157A

---
+++ 2157A.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+def print_fast(x):
+    sys.stdout.write(f"{x}\n")
+
+
+def solve():
+    
+
+
+for _ in range(int(input())):
+    print_fast(solve())
